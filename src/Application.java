f86ff731f7a6d03293774ddcc7b884d1e5ea3139
nikolaipeders <nikolaippedersen@outlook.com>
2022-03-28 15:39:05 +0200

    UI big update

---
+++ src/Application.java
@@ -0,0 +1,64 @@
+import javafx.geometry.Side;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+
+    TopBar topBar = new TopBar();
+    tabSchedule tabSchedule = new tabSchedule();
+    tabBookings tabBookings = new tabBookings();
+    public static Stage stage;
+
+    double minWidth = 1150;
+    double minHeight = 600;
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        // Starting procedures and Set the Scene
+        this.stage = stage;
+        VBox root = new VBox();
+        Scene applicationScene = new Scene(root, minWidth, minHeight);
+
+        // Add MenuBar to top of application
+        root.getChildren().add(topBar.getMenus());
+
+        // Create a tabPane
+        TabPane tabPane = new TabPane();
+        root.getChildren().add(tabPane);
+        tabPane.setSide(Side.TOP);
+        tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.UNAVAILABLE);
+
+        // Enables resizing
+        VBox.setVgrow(tabPane, Priority.ALWAYS);
+
+        // Creating the tabs
+        Tab tab1 = new Tab("Schedule");
+        Tab tab2 = new Tab("Bookings");
+
+        // Adding tabs to the tab pane
+        tabPane.getTabs().addAll(tab1, tab2);
+
+        // Fetch Tab for Schedule
+        tab1.setContent(tabSchedule.getTabSchedule(tab1));
+
+        // Fetch Tab for Bookings
+        tab2.setContent(tabBookings.getTabBookings(tab2));
+
+        // Rules
+        stage.setMinWidth(minWidth);
+        stage.setMinHeight(minHeight);
+
+        // Initialize Window
+        //stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/icon.png"))));
+        stage.setScene(applicationScene);
+        stage.setTitle("Animal Shelter Portal");
+        stage.show();
+    }
+}
+
---
+++ src/DateGenerator.java
@@ -0,0 +1,20 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class DateGenerator {
+
+    public DateGenerator() {
+
+    }
+
+    public String getDate(int week, int weekdayAsInt, int year) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.WEEK_OF_YEAR, week);
+        cal.set(Calendar.DAY_OF_WEEK, weekdayAsInt);
+        cal.set(Calendar.YEAR, year);
+
+        return sdf.format(cal.getTime());
+    }
+}
